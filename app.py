7d7384c9f8b67c89b1cbb28e37d898d4a2ca070d
PRAKRITI <prakritikhushi2004@gmail.com>
2025-11-12 01:30:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -46,5 +46,7 @@ def index():
         return render_template('index.html', devices=devices, conflicts=conflicts, network=network)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=10000)
+
